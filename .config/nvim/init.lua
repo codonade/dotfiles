c8dc06a728dc3189298604d525509306f4ec60fd
codonade <abdulrahman.haytham.shaddad@hotmail.com>
2024-03-19 17:22:46 +0300

    feat(nvim): sane options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,4 @@
 -- Bootstraps lazy.nvim and installs `config.plugins`
 require("config.lazy")
+-- Configures Neovim's core options.
+require("config.options")
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,39 @@
+-- Uses GUI colors in *supported* terminals.
+vim.opt.termguicolors = true
+-- Magically makes Neovim responsive.
+vim.opt.updatetime = 50
+
+-- Applies block cursor for very Vim mode.
+vim.opt.guicursor = ""
+-- Hides Vim modes display.
+vim.opt.showmode = false
+
+-- Displays absolute line numbers.
+vim.opt.number = true
+-- NOTE: `true` for relative lines.
+vim.opt.relativenumber = false
+-- Always displays a single sign column.
+vim.opt.signcolumn = "yes:1"
+
+-- Indents using 2 spaces.
+-- TODO: Override for some files.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Enables advanced indentation.
+vim.opt.smartindent = true
+-- "Is this a new line, or the same one?"
+vim.opt.wrap = false
+
+-- Syncs the clipboard between OS and Neovim.
+-- HMMM: Not working in WSL2 (Ubuntu)...
+vim.opt.clipboard = "unnamedplus"
+
+-- Disables all kinds of history.
+vim.opt.swapfile = false
+vim.opt.backup = false
+-- TODO: Leverage undo files. 
+vim.opt.undofile = false
+vim.opt.undodir = ""
