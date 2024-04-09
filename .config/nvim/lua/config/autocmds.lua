45c34e36b774ac2cd4a6b37a79c6900b26e49626
codonade <abdulrahman.haytham.shaddad@hotmail.com>
2024-04-09 15:51:39 +0300

    fix(nvim): indent `*.c` & `*.h` with `4` spaces

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- Override indentation for C files.
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.c", "*.h", },
+  group = vim.api.nvim_create_augroup("CodonadeIndent", {
+    -- Resets commands when reloading.
+    clear = true,
+  }),
+  callback = function()
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.shiftwidth = 4
+  end,
+})
